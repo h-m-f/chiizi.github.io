8a8f9d0d2258fe6d6ea6877a132163e95b386f92
chiizi <m33pfish@gmail.com>
2015-09-04 03:03:12 -0500

    Update script.js

--- custom-elements/script.js
+++ custom-elements/script.js
@@ -1,11 +1,11 @@
-var XMeepProto = Object.create(HTMLElement.prototype, {
+var TerminalLineProto = Object.create(HTMLElement.prototype, {
   createdCallback: {
     value: function() {
-      var t = document.querySelector("#meep-style");
+      var t = document.querySelector("#terminal-line-style");
       var clone = document.importNode(t.content, true);
       this.createShadowRoot().appendChild(clone);
     }
   }
 });
 
-var XMeep = document.registerElement("x-meep", {prototype: XMeepProto});
+var XMeep = document.registerElement("terminal-line", {prototype: TerminalLineProto});
