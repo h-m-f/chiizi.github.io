8bc2f95db656f81d17b2ba3822d16e3f98c3f209
chiizi <m33pfish@gmail.com>
2015-09-04 02:57:17 -0500

    Update script.js

--- custom-elements/script.js
+++ custom-elements/script.js
@@ -8,9 +8,4 @@ var XMeepProto = Object.create(HTMLElement.prototype{
   }
 });
 
-XMeepProto.createdCallback = function() {
-  var shadow = this.createShadowRoot();
-  shadow.innerHTML = "hello.";
-};
-
 var XMeep = document.registerElement("x-meep", {prototype: XMeepProto});
