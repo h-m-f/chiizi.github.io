ccbf11f0d04b4bec2359659699fe2f9be4000580
chiizi <m33pfish@gmail.com>
2015-09-28 11:33:09 -0500

    Add peer var

--- p2p/main.js
+++ p2p/main.js
@@ -3,6 +3,7 @@ document.body.appendChild(function() {
   script.src = "http://cdn.peerjs.com/0.3/peer.min.js";
   return script;
 });
+var peer = new Peer({key: "zn8snxn231cvunmi"});
 var conn = peer.connect('another-peers-id');
 conn.on('open', function(){
   conn.send('hi!');
