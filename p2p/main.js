41ae13bb1c57e360a01d8afcb2f5c170b9d601ee
Pcat0 <patrick.ambli@gmail.com>
2015-09-28 11:28:26 -0500

    Update main.js

--- p2p/main.js
+++ p2p/main.js
@@ -3,3 +3,13 @@ document.body.appendChild(function() {
   script.src = "http://cdn.peerjs.com/0.3/peer.min.js";
   return script;
 });
+var conn = peer.connect('another-peers-id');
+conn.on('open', function(){
+  conn.send('hi!');
+});
+peer.on('connection', function(conn) {
+  conn.on('data', function(data){
+    // Will print 'hi!'
+    console.log(data);
+  });
+});
