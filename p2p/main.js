c5199d8d5e2f6d46561a3ad858723e55da6882f0
Hans Felde <m33pfish@gmail.com>
2015-10-01 09:30:25 -0500

    DELETE FSCKING EVERYTHING

--- p2p/main.js
+++ p2p/main.js
@@ -1,11 +1,1 @@
-var peer = new Peer(prompt("Choose a name. You'll need to give this to your friends if you want them to play with you."), {key: "zn8snxn231cvunmi"});
-peer.on("connection", function(conn) {
-  conn.on("data", function(data) {
-    // Will print 'hi!'
-    console.log(data);
-  });
-});
-var conn = peer.connect(prompt("What is your friend's in-game name?"));
-conn.on("open", function() {
-  conn.send("hi!");
-});
+
