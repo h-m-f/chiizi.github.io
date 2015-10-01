8452e2548f865475e932088fe0655fdd2dd103e9
Hans Felde <m33pfish@gmail.com>
2015-10-01 08:37:35 -0500

    Update main.js

--- p2p/main.js
+++ p2p/main.js
@@ -1,15 +1,10 @@
-document.body.appendChild((function() {
-  var script = document.createElement("script");
-  script.src = "peer.min.js";
-  return script;
-})());
+var peer = new Peer(prompt("Choose a name. You'll need to give this to your friends if you want them to play with you."), {key: "zn8snxn231cvunmi"});
 peer.on("connection", function(conn) {
   conn.on("data", function(data) {
     // Will print 'hi!'
     console.log(data);
   });
 });
-var peer = new Peer(prompt("Choose a name. You'll need to give this to your friends if you want them to play with you."), {key: "zn8snxn231cvunmi"});
 var conn = peer.connect(prompt("What is your friend's in-game name?"));
 conn.on("open", function() {
   conn.send("hi!");
