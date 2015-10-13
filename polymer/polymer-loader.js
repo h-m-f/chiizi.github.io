e9dea67c7d77cf295b90cb6a35aeed9394904158
Hans Felde <m33pfish@gmail.com>
2015-10-13 12:55:30 -0500

    Create polymer-loader.js

---
+++ polymer/polymer-loader.js
@@ -0,0 +1,9 @@
+var importPolymer = function(ver) {
+  arguments = Array.slice.call(arguments, 1);
+  arguments.map(function(e) {
+    var link = document.createElement("link");
+    link.rel = "import";
+    link.href = "https://www.polymer-project.org/" + ver + "/components/" + e + "/" + e + ".html";
+    document.head.appendChild(link);
+  });
+};
