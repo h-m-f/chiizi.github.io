9e37c5c6842432dc23ac80a9af66db1b4f29d707
Hans Felde <m33pfish@gmail.com>
2015-10-13 22:43:41 -0500

    Update polymer-loader.js

--- js/polymer-loader.js
+++ js/polymer-loader.js
@@ -1,6 +1,5 @@
 var importPolymer = function(ver) {
-  arguments = Array.prototype.slice.call(arguments, 1);
-  arguments.map(function(e) {
+  arguments = Array.prototype.slice.call(arguments, 1).map(function(e) {
     var link = document.createElement("link");
     link.rel = "import";
     link.href = "https://cdn.rawgit.com/download/polymer-cdn/" + ver + "/lib/" + e + "/" + e + ".html";
