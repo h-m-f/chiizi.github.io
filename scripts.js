3b42e45399385f2db31d78c4ef482f2a3ad13ba1
Hans Felde <m33pfish@gmail.com>
2015-10-13 22:46:39 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 var importScript = function() {
   arguments = Array.prototype.slice.call(arguments, 1).map(function(e) {
     var script = document.createElement("script");
-    script.src = e;
+    script.src = "/js/" + e + ".js";
     document.head.appendChild(script);
   });
 };
