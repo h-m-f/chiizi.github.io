3a475f015b60cc708dd29e0ef4de6f17c36f1b62
Hans Felde <m33pfish@gmail.com>
2015-10-22 22:48:08 -0500

    Update import.js

--- terminal/import.js
+++ terminal/import.js
@@ -1,5 +1,5 @@
 send.register("import", path => {
-  var process = (path => path.replace(/^(?:\/\/)/, "https:") + ".js");
+  var process = (path => path.replace(/^\/\//, "https://") + ".js");
   var script = document.createElement("script");
   script.src = process(path);
   document.body.appendChild(script);
