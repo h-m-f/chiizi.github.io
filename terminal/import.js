6d69e41f706d912650cb93e0f2edb7f6f8ca7ec7
Hans Felde <m33pfish@gmail.com>
2015-10-26 11:30:06 -0500

    Update import.js

--- terminal/import.js
+++ terminal/import.js
@@ -1,6 +1,7 @@
-send.register("import", path => {
+send.register("import", (path, io) => {
   var process = (path => path.replace(/^\/\//, "https://") + ".js");
   var script = document.createElement("script");
   script.src = process(path);
+  io(path + " imported as " + process);
   document.body.appendChild(script);
 });
