f386a5216324dce54236c251c6c094dccfc3681e
Hans Felde <m33pfish@gmail.com>
2015-10-31 22:05:58 -0500

    Add multi-script support

--- scripts.js
+++ scripts.js
@@ -1,8 +1,20 @@
-var importScript = function() {
-  arguments = Array.prototype.slice.call(arguments, 0).map(function(e) {
+var importScript = function(soa, f) {
+  if (soa.constructor == Array) {
+    var loads = soa.slice(0).fill(false);
+    soa.map(function(e, i) {
+      var script = document.createElement("script");
+      script.src = "/js/" + e + ".js";
+      script.onload = function() {
+        loads[i] = true;
+        if (Math.min.apply(Math, loads)) {
+          f();
+        }
+      };
+    });
+  } else {
     var script = document.createElement("script");
-    script.src = "/js/" + e[0] + ".js";
-    script.onload = e[1];
+    script.src = "/js/" + soa + ".js";
+    script.onload = f;
     document.head.appendChild(script);
-  });
+  }
 };
