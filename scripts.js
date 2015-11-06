bf1ca08b1445f38065ed3ffc54f192e8ba8098cd
Hans Felde <m33pfish@gmail.com>
2015-11-06 17:05:36 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
-var importScript = function(soa, f) {
+var importScript = function(soa, f, sza, bzf) {
+  sza = sza != undefined ? sza : ".js";
   if (soa.constructor == Array) {
     var loads = soa.slice(0).fill(false);
     soa.map(function(e, i) {
       var script = document.createElement("script");
-      script.src = "/js/" + e + ".js";
+      script.src = "/js/" + e + sza;
       script.onload = function() {
         loads[i] = true;
         if (Math.min.apply(Math, loads)) {
@@ -13,8 +14,16 @@ var importScript = function(soa, f) {
     });
   } else {
     var script = document.createElement("script");
-    script.src = "/js/" + soa + ".js";
+    if (soa[0] == "/") {
+      script.src = "https://" + (/^\/[0-9A-Za-z-.]+/).exec(soa)[0].split("").slice(1).join("").split(".").reverse().join(".") + (/^[0-9A-Za-z-./]+?(?=[0-9A-Za-z-.]+\/$)/).exec(soa.split("").reverse().join(""))[0].split("").reverse().join("") + sza;
+    } else if (soa[0] == "~") {
+      script.src = "/" + soa.substr(1) + sza;
+    } else if (soa[0] == "!") {
+      script.src = importScript(soa.substr(1), f, "", true).src;
+    } else {
+      script.src = "/js/" + soa + sza;
+    }
     script.onload = f;
-    document.head.appendChild(script);
+    return bzf ? script : document.head.appendChild(script), script;
   }
 };
