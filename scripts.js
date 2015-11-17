1c49c54980a27839490ffe71f8993862f4e297f6
Hans Felde <m33pfish@gmail.com>
2015-11-16 21:51:26 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,6 @@
 var importScript = function(soa, f, sza, bzf) {
   sza = sza != undefined ? sza : ".js";
-  if (soa.constructor == Array) {
-    var loads = soa.slice(0).fill(false);
-    soa.map(function(e, i) {
-      var script = document.createElement("script");
-      script.src = "/js/" + e + sza;
-      script.onload = function() {
-        loads[i] = true;
-        if (Math.min.apply(Math, loads)) {
-          f();
-        }
-      };
-    });
-  } else {
+  if (this.gotten.indexOf(soa) == -1) {
     var script = document.createElement("script");
     if (soa[0] == "/") {
       script.src = "https://" + (/^\/[0-9A-Za-z-.]+/).exec(soa)[0].split("").slice(1).join("").split(".").reverse().join(".") + (/^[0-9A-Za-z-./]+?(?=[0-9A-Za-z-.]+\/$)/).exec(soa.split("").reverse().join(""))[0].split("").reverse().join("") + sza;
@@ -44,3 +32,5 @@ var importJSON = function(url) {
     xhr.send();
   });
 };
+
+importScript.gotten = [];
