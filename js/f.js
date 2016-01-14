63ced1be0b1a05024718f861562b866b0ce892ec
Hans Felde <m33pfish@gmail.com>
2016-01-14 10:34:00 -0600

    Update f.js

--- js/f.js
+++ js/f.js
@@ -1,10 +1,14 @@
 var F = function() {
-  this.takeOver = function() {
-    document.getElementById("f")
-  }
+  $ = document.querySelector
+  $$ = document.querySelectorAll
+  $new = document.createElement
+  var e = document.getElementById("f") || $("body").appendChild(e => e.id = "f")($new("div"))
+  
+  this.log = s =>
+    
   this.appLoad = function(s) {
     importJSON(s + "/cfg.json").then(o => {
-      
+      importScript(s + o.init)
     })
   }
   return this
