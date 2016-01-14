3ccb8d0d7ba30f28fb811e78170b311fde3f4789
Hans Felde <m33pfish@gmail.com>
2016-01-14 10:40:03 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -18,11 +18,27 @@ var importScript = function(soa, f, sza, bzf) {
 
 importScript.gotten = [];
 
+var importText = function(url) {
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', url, true);
+    xhr.responseType = "text";
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status == 200) {
+        resolve(xhr.response);
+      } else {
+        reject(status);
+      }
+    };
+    xhr.send();
+  });
+};
 var importJSON = function(url) {
   return new Promise(function(resolve, reject) {
     var xhr = new XMLHttpRequest();
     xhr.open('get', url, true);
-    xhr.responseType = 'json';
+    xhr.responseType = "json";
     xhr.onload = function() {
       var status = xhr.status;
       if (status == 200) {
