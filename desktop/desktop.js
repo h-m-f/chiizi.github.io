5f8cfc5e3ec9e2bc0176b886049e9fdd2f761fcf
Hans Felde <m33pfish@gmail.com>
2016-02-02 10:28:15 -0600

    Update desktop.js

--- desktop/desktop.js
+++ desktop/desktop.js
@@ -6,32 +6,37 @@ document.documentElement.addEventListener("click", () =>
   document.documentElement.webkitRequestFullscreen());
 
 var windows = [];
-var newWindow = (id, title) => {
-  var md = e => {
-    offX = e.clientX - parseInt(elem.offsetLeft);
-    offY = e.clientY - parseInt(elem.offsetTop);
-    window.addEventListener("mousemove", mm, true);
-    window.addEventListener("mouseup", mu, true);
-  };
-  var mu = () => {
-    window.removeEventListener("mousemove", mm, true);
-  };
-  var mm = e => {
-    elem.style.left = (e.clientX - offX) + "px";
-    elem.style.top = (e.clientY - offY) + "px";
-  };
-  var offX;
-  var offY;
-  
-  var elem = document.createElement("div");
-  elem.classList.add("win");
-  elem.classList.add("initial-size");
-  elem.innerHTML = `<div class="wintop" title="${title}" id="${id}">
+var newWindow = (options) => {
+  with (options) {
+    var md = e => {
+      offX = e.clientX - parseInt(elem.offsetLeft);
+      offY = e.clientY - parseInt(elem.offsetTop);
+      window.addEventListener("mousemove", mm, true);
+      window.addEventListener("mouseup", mu, true);
+    };
+    var mu = () => {
+      window.removeEventListener("mousemove", mm, true);
+    };
+    var mm = e => {
+      elem.style.left = (e.clientX - offX) + "px";
+      elem.style.top = (e.clientY - offY) + "px";
+    };
+    var offX;
+    var offY;
+    
+    var elem = document.createElement("div");
+    elem.classList.add("win");
+    elem.classList.add("initial-size");
+    elem.innerHTML = `<div class="wintop" title="${title}" id="${id}">
   <div class="icon reload"></div><div class="icon minimize"></div><div class="icon resize"></div><div class="icon close"></div>
 </div>`;
-  elem.querySelector(".wintop").addEventListener("mousedown", md, false);
-  return elem;
+    elem.querySelector(".wintop").addEventListener("mousedown", md, false);
+    return elem;
+  }
 };
 
-var login = newWindow("login", "login");
+var login = newWindow({
+  id: "test",
+  "title": "test"
+});
 document.body.appendChild(login);
